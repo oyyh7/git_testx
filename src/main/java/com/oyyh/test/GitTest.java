55f2e39579c0ca044d877168ff0d313902c3dd5b
oyyh <oyyh@7777.com>
2021-09-29 23:13:12 +0800

    GitTest First Commit

---
+++ src/main/java/com/oyyh/test/GitTest.java
@@ -0,0 +1,13 @@
+package com.oyyh.test;
+
+/**
+ * @Auther: oyyh
+ * @Date: 2021/9/29 - 09 - 29 - 23:08
+ * @Description: com.oyyh.test
+ * @version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
